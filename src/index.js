d735ea4f195cf5c3190fb90a26cd70063388653f
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-26 18:07:07 -0300

    21 the Store
    21 the Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider  } from 'react-redux';
+import rootReducer from './reducers';
+import { createStore } from 'redux';
 
+
+const store = createStore(rootReducer);
+
+console.log('store.getState()', store.getState() );
 
 ReactDOM.render(
-    <App/> , document.getElementById('root')
+    <Provider store = {store} >
+        <App/> 
+    </Provider>
+    ,  document.getElementById('root')
 );
 
