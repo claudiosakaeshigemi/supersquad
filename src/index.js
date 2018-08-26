0b704720db5844a6ff233396c37df7d269a1b053
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-26 18:28:44 -0300

    22 The First Action
    22 The First Action

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './components/App';
 import { Provider  } from 'react-redux';
 import rootReducer from './reducers';
 import { createStore } from 'redux';
-
+import { addCharacterById } from './actions';
 
 const store = createStore(rootReducer);
 
 console.log('store.getState()', store.getState() );
+store.subscribe( () => console.log('store',store.getState()) );
+store.dispatch(addCharacterById(2) );
 
 ReactDOM.render(
     <Provider store = {store} >
