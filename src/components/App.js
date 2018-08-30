b10c2c344a1ddfdbf6b6a2dbe1f84cbfcb397303
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-30 20:28:38 -0300

    27 Creating a Hero List
    27 Creating a Hero List

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React , { Component } from 'react';
 import CharacterList from './CharacterList';
 import '../styles/index.css';
+import HeroList from './heroList';
+
+
 
 class App extends Component {
     render () {
         return ( 
             <div className= "App"> 
                 <h2> SuperSquad</h2>
-                <CharacterList/>
+                <div className="col-md-6">
+                    <CharacterList/>
+                </div>
+
+                <div className="col-md-6">
+                    <HeroList/> 
+                </div>
+                
             </div>
         )
     }
